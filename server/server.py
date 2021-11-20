684a7ab31013a6bfc4c53525de7671193baf6950
Paulo Ubirajara de Mattos Neto <ubirajara3@hotmail.com>
2021-11-19 21:46:16 -0300

    Criar banco de dados ao iniciar server

--- server/server.py
+++ server/server.py
@@ -1,16 +1,27 @@
 from socket import AF_INET, SOCK_STREAM, socket
 from threading import Thread
 from typing import Union
+from os.path import isfile
+from json import dumps
 
 from server_handler import ServerHandler
 
 
 class Server(Thread):
     addr: Union[str, int]
+    database_name = 'database.json'
 
     def __init__(self, host: str, port: int):
         Thread.__init__(self)
         self.addr = (host, port)
+        self.create_database_if_not_exists()
+
+    def create_database_if_not_exists(self):
+        if not isfile(self.database_name):
+            default_database = {'users': {}, 'rules': []}
+
+            with open(file=self.database_name, mode='w') as file:
+                file.write(dumps(default_database))
 
     def run(self):
         with socket(AF_INET, SOCK_STREAM) as s:
